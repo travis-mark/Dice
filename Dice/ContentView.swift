4a2ae9978fefbb87abd7888d8f1bff98b2ef406d
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-06-13 22:32:33 -0400

    Initial Commit

---
+++ Dice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dice
+//
+//  Created by Travis Luckenbaugh on 6/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Travis Luckenbaugh on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
